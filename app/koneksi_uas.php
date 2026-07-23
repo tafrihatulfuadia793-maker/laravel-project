46ce4e8106d686f7ea28172503c847e7ebadbc9b
tafrihatulfuadia793-maker <tafrihatulfuadia793@gmail.com>
2026-07-23 12:46:36 +0700

    updatedd

--- app/koneksi_uas.php
+++ app/koneksi_uas.php
@@ -1,14 +1,13 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "kampus";
+$host = env('DB_HOST');
+$user = env('DB_USERNAME');
+$password = env('DB_PASSWORD');
+$database = env('DB_DATABASE');
+$port = env('DB_PORT');
 
-$conn = mysqli_connect($host, $user, $password, $database);
+$conn = mysqli_connect($host, $user, $password, $database, $port);
 
 if (!$conn) {
     die("Koneksi gagal : " . mysqli_connect_error());
 }
-
-?>
